6c65cef38a69925621d3d9f4233dfe05d4a0d29b
Ben Sutherland <ben.sutherland.1@ulaval.ca>
2015-03-24 22:02:53 -0400

    commenting and user-selected criteria
    Small improvements in commenting, and defining with user-selected criteria.

--- Rqtl/shading_chr-rqtl-genomescan.R
+++ Rqtl/shading_chr-rqtl-genomescan.R
@@ -1,23 +1,27 @@
 ############PLOT GREY WHITE GREY PANEL ON Rqtl GENOME SCAN#######
-# replace "all.out.0.nocov" with any scanone model
-# in the object with chromosome lengths given in sfqtl$geno
-# collect the length of each chr
+#currently works for a 42 chromosome plot
+# replace "out.am" with any scanone model
+# replace "pheno.lev.a" with chromosome-wide significance level vector
+# replace "operm.am" with genome-wide significance level vector
+
+# in the read.cross object with chromosome lengths (replace sfqtl)
+# to collect the length of each chr
 len.chr = NULL
 for(i in names(sfqtl$geno)){
   len.chr = c(len.chr, max(sfqtl$geno[[i]]$map))  
 }
 
-len.chr
-# add buffer space (gaps between blocks)
-buffer.leng <- len.chr + 25
+len.chr 
+
+buffer.leng <- len.chr + 25 # add buffer space (gaps between blocks)
 cu.buffer.leng <- cumsum(buffer.leng) #finds the cumulative position for each dividing block
-# find x-axis positions
-left.grey <- c(0, cu.buffer.leng[c(FALSE, TRUE)])
-right.grey <- c(cu.buffer.leng[c(TRUE, FALSE)])
 
+left.grey <- c(0, cu.buffer.leng[c(FALSE, TRUE)]) # find x-axis positions, left side
+right.grey <- c(cu.buffer.leng[c(TRUE, FALSE)])   # find x-axis positions, right side
 
+# plot scanone object (replace "out.am")
 plot(out.am, alternate.chrid=T, type="n", lodcolumn=6, ylim = c(0,8), ylab = "Length (cm) T2",
-     xlab = "")
+     xlab = "") #choose lodcolumn
 rect(xleft = left.grey[0:21], xright= right.grey[0:21],
      ybottom = 0, ytop = 11,
      col = c("lightgrey"),
